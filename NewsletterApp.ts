6eda76e65df8c94dc1945f2277374a08d2edaf88
kikuchi <nyoronyoro0327@gmail.com>
2024-06-28 16:54:37 +0900

    add newsletterApp.ts

--- NewsletterApp.ts
+++ NewsletterApp.ts
@@ -6,21 +6,17 @@ import {
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 import { GenerateCommand } from './commands/GenerateCommand';
-import { settings } from './settings/settings';
 
 export class NewsletterApp extends App {
 	constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
 		super(info, logger, accessors);
 	}
 
-	public async extendConfiguration(configuration: IConfigurationExtend) {
-		await Promise.all([
-			...settings.map((setting) =>
-				configuration.settings.provideSetting(setting)
-			),
-			configuration.slashCommands.provideSlashCommand(
-				new GenerateCommand(this)
-			),
-		]);
+	protected async extendConfiguration(
+		configuration: IConfigurationExtend
+	): Promise<void> {
+		await configuration.slashCommands.provideSlashCommand(
+			new GenerateCommand(this)
+		);
 	}
 }
