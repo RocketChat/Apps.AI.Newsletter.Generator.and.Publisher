761240ab7fb43d702289e6c383206ba41fb00f0b
kikuchi <nyoronyoro0327@gmail.com>
2024-06-28 14:24:34 +0900

    add input

---
+++ types/NewsletterInput.ts
@@ -0,0 +1,9 @@
+export interface NewsletterInput {
+	product_name: string;
+	new_features: string;
+	benefits: string;
+	faq: string;
+	additional_info: string;
+	team_name: string;
+	style: 'maintaining structure' | 'free-form paragraphs';
+}
