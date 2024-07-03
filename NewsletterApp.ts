1cf139a704e1eb483d5808f43e426bd64a8830be
kikuchi <nyoronyoro0327@gmail.com>
2024-07-03 10:01:57 +0900

    add import of app settings

--- NewsletterApp.ts
+++ NewsletterApp.ts
@@ -6,17 +6,21 @@ import {
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 import { NewsletterCommand } from './commands/GenerateCommand';
+import { settings } from './settings/settings';
 
 export class NewsletterApp extends App {
 	constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
 		super(info, logger, accessors);
 	}
 
-	protected async extendConfiguration(
-		configuration: IConfigurationExtend
-	): Promise<void> {
-		await configuration.slashCommands.provideSlashCommand(
-			new NewsletterCommand(this)
-		);
+	public async extendConfiguration(configuration: IConfigurationExtend) {
+		await Promise.all([
+			...settings.map((setting) =>
+				configuration.settings.provideSetting(setting)
+			),
+			configuration.slashCommands.provideSlashCommand(
+				new NewsletterCommand(this)
+			),
+		]);
 	}
 }
